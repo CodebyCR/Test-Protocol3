3bcf317f82f675735183976f9ad2d488239121b4
Christoph <christoph_rohde@icloud.com>
2024-04-14 18:20:19 +0200

    Initial Commit

---
+++ Test-Protocol3/Test_Protocol3App.swift
@@ -0,0 +1,17 @@
+//
+//  Test_Protocol3App.swift
+//  Test-Protocol3
+//
+//  Created by Christoph Rohde on 14.04.24.
+//
+
+import SwiftUI
+
+@main
+struct Test_Protocol3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
